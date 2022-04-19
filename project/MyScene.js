0437a3d910c5adabb239f723cad5dedb7e465bc7
Bruno Pascoal <brunompp98@gmail.com>
2022-04-19 14:25:17 +0100

    project base code

---
+++ project/MyScene.js
@@ -0,0 +1,84 @@
+import { CGFscene, CGFcamera, CGFaxis, CGFappearance } from "../lib/CGF.js";
+import { MyPlane } from "./MyPlane.js";
+
+/**
+* MyScene
+* @constructor
+*/
+export class MyScene extends CGFscene {
+    constructor() {
+        super();
+    }
+    init(application) {
+        super.init(application);
+        this.initCameras();
+        this.initLights();
+
+        //Background color
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        this.gl.clearDepth(100.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.depthFunc(this.gl.LEQUAL);
+
+        this.setUpdatePeriod(50);
+        
+        this.enableTextures(true);
+
+        //Initialize scene objects
+        this.axis = new CGFaxis(this);
+        this.plane = new MyPlane(this, 20, 0,1,0,1);
+
+        //Objects connected to MyInterface
+        this.displayAxis = true;
+    }
+    initLights() {
+        this.lights[0].setPosition(15, 2, 5, 1);
+        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].enable();
+        this.lights[0].update();
+    }
+    initCameras() {
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(30,30,30), vec3.fromValues(0, 0, 0));
+    }
+
+    setDefaultAppearance() {
+        this.setAmbient(0.2, 0.4, 0.8, 1.0);
+        this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+        this.setSpecular(0.2, 0.4, 0.8, 1.0);
+        this.setEmission(0,0,0,1);
+        this.setShininess(10.0);
+    }
+
+    // called periodically (as per setUpdatePeriod() in init())
+    update(t){
+        //To be done...
+    }
+
+    display() {
+        // ---- BEGIN Background, camera and axis setup
+        // Clear image and depth buffer everytime we update the scene
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        // Initialize Model-View matrix as identity (no transformation
+        this.updateProjectionMatrix();
+        this.loadIdentity();
+        // Apply transformations corresponding to the camera position relative to the origin
+        this.applyViewMatrix();
+
+        // Draw axis
+        if (this.displayAxis)
+            this.axis.display();
+
+        this.setDefaultAppearance();
+
+        // ---- BEGIN Primitive drawing section
+        this.pushMatrix();
+        this.scale(50,1,50);
+        this.rotate(-Math.PI*0.5, 1,0,0);
+        this.plane.display();
+        this.popMatrix();
+        // ---- END Primitive drawing section
+    }
+}
