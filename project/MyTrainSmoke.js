62de272222392b75c2a9081442de20d529721928
Bruno Pascoal <brunompp98@gmail.com>
2022-06-01 22:30:47 +0100

    Multiple smokes applied

--- project/MyTrainSmoke.js
+++ project/MyTrainSmoke.js
@@ -5,10 +5,18 @@ export class MyTrainSmoke extends CGFobject{
     constructor(scene){
         super(scene);
 
-        this.cloud = new MySphere(scene, 20, 20);
+        this.random1 = Math.floor(Math.random() * (20 - 4 + 1)) + 4;
+        this.random2 = Math.floor(Math.random() * (20 - 4 + 1)) + 4;
+
+        this.cloud = new MySphere(scene, this.random1, this.random2);
+        this.cloud2 = new MySphere(scene, this.random2, this.random1);
+        this.cloud3 = new MySphere(scene, this.random1, this.random1);
+        this.cloud4 = new MySphere(scene, this.random2, this.random2);
+
 
         this.initialPosition = {x: 0, y: 0, z: 0};
         this.scaleFactor = 0;
+        this.randomNumber();
         this.initMaterials(scene);
     }
 
@@ -34,23 +42,51 @@ export class MyTrainSmoke extends CGFobject{
 
     updateCloud() {
 
-        if(this.scaleFactor > 0.4){
+        if(this.scaleFactor > 0.7){
+            this.randomNumber();
             this.resetCloud();
         } else {
             this.initialPosition.y += 0.01;
             this.initialPosition.z -= 0.01;
-            this.scaleFactor += 0.001;
+            this.scaleFactor += 0.002;
         }
-        console.log(this.scaleFactor + " " + this.initialPosition.y + " " + this.initialPosition.z);
     }
 
-    display(){
+    randomNumber() {
+        this.cloud1Number = (Math.floor(Math.random() * (10 - 1 + 1)) + 1) / 100;
+        this.cloud2Number = (Math.floor(Math.random() * (10 - 1 + 1)) + 1) / 100;
+        this.cloud3Number = (Math.floor(Math.random() * (10 - 1 + 1)) + 1) / 100;
+        this.cloud4Number = (Math.floor(Math.random() * (10 - 1 + 1)) + 1) / 100;
+    }
+
+    display(){  
 
         this.scene.pushMatrix();
         this.cloudTexture.apply();
         this.scene.translate(this.initialPosition.x, this.initialPosition.y + 4.3, this.initialPosition.z + 2.1);
-        this.scene.scale(0.2 + this.scaleFactor,0.2 + this.scaleFactor,0.2 + this.scaleFactor);
+        this.scene.scale(this.cloud1Number + this.scaleFactor,this.cloud1Number + this.scaleFactor,this.cloud1Number + this.scaleFactor);
         this.cloud.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.cloudTexture.apply();
+        this.scene.translate(this.initialPosition.x, this.initialPosition.y + 5, this.initialPosition.z + 2.1);
+        this.scene.scale(this.cloud2Number + this.scaleFactor,this.cloud2Number + this.scaleFactor,this.cloud2Number + this.scaleFactor);
+        this.cloud2.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.cloudTexture.apply();
+        this.scene.translate(this.initialPosition.x, this.initialPosition.y + 4.5, this.initialPosition.z + 2.1);
+        this.scene.scale(this.cloud3Number + this.scaleFactor,this.cloud3Number + this.scaleFactor,this.cloud3Number + this.scaleFactor);
+        this.cloud3.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.cloudTexture.apply();
+        this.scene.translate(this.initialPosition.x, this.initialPosition.y + 4.1, this.initialPosition.z + 2.2);
+        this.scene.scale(this.cloud4Number + this.scaleFactor,this.cloud4Number + this.scaleFactor,this.cloud4Number + this.scaleFactor);
+        this.cloud4.display();
         this.scene.popMatrix();
 
     }
